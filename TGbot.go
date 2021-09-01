a3f05b52b802b57f46d188518f457db0f1fb711c
DKeys666 <58102970+DKeys666@users.noreply.github.com>
2021-09-01 20:54:16 +0300

    Update TGbot.go

--- TGbot.go
+++ TGbot.go
@@ -1,42 +1,78 @@
 package main
 
 import (
+	"context"
+	//"encoding/json"
+//	"fmt"
 	"github.com/Syfaro/telegram-bot-api"
+	sdk "github.com/TinkoffCreditSystems/invest-openapi-go-sdk"
 	"log"
-	
+	//"os"
+
+	"time"
 )
 
-func telegramBot() {
-	//подключаемся к боту с помощью токена
-	bot, err := tgbotapi.NewBotAPI("1935733666:AAGj-bDMkUR6DZIqwiNjhDJCbomieEkVZYo")
+const (
+	tgtoken   = "t.nbVvVcHGxEFof8uIWo7uywaAubEo2aULyhzPLXjuesSe9_Y8TzQUar37G_Hk1fsaY1xQMnrfFf0DGzk4Nj3R5g"
+	timeout = time.Second * 3
+	url     = "https://api-invest.tinkoff.ru/openapi/sandbox/sandbox/register"
+)
+
+func main() {
+	ReturnMessage()
+}
+
+func ReturnMessage() {
+	bot, err := tgbotapi.NewBotAPI(tgtoken)
+
 	if err != nil {
 		log.Panic(err)
 	}
-	bot.Debug = true
-	log.Printf("Aythorized on account %s", bot.Self.UserName)
 
-	//Устанавливаем время обновления
+	bot.Debug = false
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	//получаем обновления от бота
 	updates, err := bot.GetUpdatesChan(u)
-	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
-	}
 
+	if err != nil {
+		log.Panic(err)
+	}
+	// В канал updates будут приходить все новые сообщения.
+	for update := range updates {
+		// Создав структуру - можно её отправить обратно боту
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+		bot.Send(msg)
+	}
 }
 
+func ticker() { //cм материал 3
+	client := sdk.NewRestClient(tgtoken)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	//получение инструмента по тикеру
+	instruments, err := client.InstrumentByTicker(ctx, "TCS")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("%+v\n", instruments)
+
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+}
 
 
 /*
 1.	Создать бота+
 2.	Библиотеки+
 2.1.	Библеотека для ТГбота+
-2.2.	Библиотека для streaming OpenAPI
-3.	Связаться со Stream OpenAPI
+2.2.	Библиотека для streaming OpenAPI+
+3.	Связаться со Stream OpenAPI+
 4.	Получить котировки
 4.1.	Вытащить данные из OpenAPI
 4.2.	Получение изменений в реальном времени
@@ -50,5 +86,13 @@ func telegramBot() {
 
 ____Материалы___________________________________________________
 
-Тинькофф Streaming OpenAPI:
+1. Тинькофф Streaming OpenAPI:
 https://tinkoffcreditsystems.github.io/invest-openapi/marketdata/
+
+2. Код для инфестиций:
+https://github.com/TinkoffCreditSystems/invest-openapi-go-sdk/blob/master/examples/main.go
+
+
+3. sdk tinkoff api
+//стр. 169 https://github.com/TinkoffCreditSystems/invest-openapi-go-sdk/blob/master/examples/main.go#L169
+ */
